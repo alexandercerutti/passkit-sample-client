cfd831efa7ed12fff5459e4b0da634d3a224c2aa
alexandercerutti <cerutti.alexander@gmail.com>
2018-08-20 01:37:26 +0200

    Removed device rotation

---
+++ Passkit-sample-client/DetailsModalVC.swift
@@ -0,0 +1,35 @@
+//
+//  DetailsModalVCViewController.swift
+//  Passkit-sample-client
+//
+//  Created by Alexander Cerutti on 19/08/18.
+//  Copyright © 2018 Alexander Cerutti. All rights reserved.
+//
+
+import UIKit
+
+class DetailsModalVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
