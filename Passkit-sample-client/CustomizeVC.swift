de11a904c4b83bfd8ca5fc22a4b68bf637830b4e
alexandercerutti <cerutti.alexander@gmail.com>
2018-09-28 16:03:56 +0200

    Added View Controllers for customizing pass output

---
+++ Passkit-sample-client/CustomizeVC.swift
@@ -0,0 +1,30 @@
+//
+//  CustomizeViewController.swift
+//  Passkit-sample-client
+//
+//  Created by Alexander Cerutti on 23/09/18.
+//  Copyright © 2018 Alexander Cerutti. All rights reserved.
+//
+
+import UIKit
+
+class CustomizeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Passkit-sample-client/editVC.swift
@@ -0,0 +1,30 @@
+//
+//  editVC.swift
+//  Passkit-sample-client
+//
+//  Created by Alexander Cerutti on 25/09/18.
+//  Copyright © 2018 Alexander Cerutti. All rights reserved.
+//
+
+import UIKit
+
+class editVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
